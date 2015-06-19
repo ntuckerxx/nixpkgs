cf6e44b90e42263639a8ba81b8d11035e898d155
Neal Tucker <neal@bandcamp.com>
2015-06-18 22:01:41 -0700

    use mp4v2 v2.0.0, patches no longer necessary

--- pkgs/development/libraries/mp4v2/default.nix
+++ pkgs/development/libraries/mp4v2/default.nix
@@ -1,19 +1,13 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "mp4v2-1.9.1p4";
+  name = "mp4v2-2.0.0";
 
   src = fetchurl {
     url = "http://mp4v2.googlecode.com/files/${name}.tar.bz2";
-    sha256 = "1d73qbi0faqad3bpmjfr4kk0mfmqpl1f43ysrx4gq9i3mfp1qf2w";
+    sha1 = "193260cfb7201e6ec250137bcca1468d4d20e2f0";
   };
 
-  # From Handbrake
-  # mp4v2 doesn't seem to be actively maintained any more :-/
-  patches = [
-    ./A00-nero-vobsub.patch ./A01-divide-zero.patch ./A02-meaningful-4gb-warning.patch
-    ./P00-mingw-dllimport.patch
-  ];
   # `faac' expects `mp4.h'.
   postInstall = "ln -s mp4v2/mp4v2.h $out/include/mp4.h";
 
